285719e710429ba6d51d54cdaa0413d0a7e94ab0
chengxuntv-lgtm <chengxuntv@gmail.com>
2026-05-07 12:59:31 +0800

    重新上傳

--- scripts/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "開始更新系統並安裝 NGINX 與 PHP..."
-sudo apt update && sudo apt upgrade -y
-sudo apt install nginx php-fpm -y
-echo "安裝完成！"
--- web-src/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<h1>歡迎來到誠訊工作室 (Chengxun Studio)</h1>";
-echo "<p>伺服器已成功架設並順利運作中。</p>";
-echo "<p>系統時間：" . date("Y-m-d H:i:s") . "</p>";
-?>
